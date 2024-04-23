81915cd655f3fdf198132fe392fd34adeafdce19
JuanDiegoMontero <monteromjd@gmail.com>
2024-04-23 11:33:51 -0500

    Recaptcha añadido

--- frontend/src/components/client/Login.jsx
+++ frontend/src/components/client/Login.jsx
@@ -1,7 +1,14 @@
 import * as React from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 
 export default function Login(){
+    const captcha = React.useRef(null);
+    const onChange = () =>{
+        if(captcha.current.getValue()){
+            console.log("El usuario no es un robot")
+        }
 
+    }
     return(
         <div className="bg-gradient-to-b from-red-light from-75% to-red-dark py-20 relative px-10 rounded-2xl ">
             <div>
@@ -14,7 +21,11 @@ export default function Login(){
                     <input className="mt-7 w-full rounded p-3 bg-white" placeholder="Contraseña" type="password"></input>
                 </div>
                 <div className="recaptcha">
-                    <input className="" placeholder="CAPCHA" type="password"></input>
+                    <ReCAPTCHA
+                        ref={captcha}
+                        sitekey="6LfEr8QpAAAAAHkHnuDZebwy-ZRwIYKLoVA5MmyR"
+                        onChange={onChange}
+                    />,
                 </div >
                 <div className="">
                     <button className="hover:bg-blue-light bg-blue-dark rounded-2xl m-5 mt-10 px-20 py-3 text-white font-semibold text-2xl font-title">Iniciar sesión</button>
