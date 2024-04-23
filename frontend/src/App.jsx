6deabe77ab9f66239346e4234e7a76bebc23a858
JuanDiegoMontero <monteromjd@gmail.com>
2024-04-23 11:03:10 -0500

    Ahora si colocado

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Home from "./pages/client/Home"
+import Login from "./components/client/Login.jsx"
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/*" element={<Home />} />
+      
+        <Route  path="/" element={<div className="w-full flex items-center justify-center h-screen"> <Login /> </div>} />
+
     </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/components/client/Login.jsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+
+export default function Login(){
+
+    return(
+        <div className="bg-gradient-to-b from-red-light from-75% to-red-dark py-20 relative px-10 rounded-2xl ">
+            <div>
+                <div className=" w-20 h-20 bg-white rounded-full ml-auto mr-auto " >
+                </div>
+                <div>
+                    <input className="w-full rounded p-3 bg-white mt-20 " placeholder="Usuario o correo electrónico"></input>
+                </div>
+                <div>
+                    <input className="mt-7 w-full rounded p-3 bg-white" placeholder="Contraseña" type="password"></input>
+                </div>
+                <div className="recaptcha">
+                    <input className="" placeholder="CAPCHA" type="password"></input>
+                </div >
+                <div className="">
+                    <button className="hover:bg-blue-light bg-blue-dark rounded-2xl m-5 mt-10 px-20 py-3 text-white font-semibold text-2xl font-title">Iniciar sesión</button>
+                </div>
+                </div>
+                <hr className=" bg-white border-0 h-0.5 w-full" >
+
+                </hr>
+                <div className="text-sm text-white flex justify-center ">
+                    ¿Olvidó su contraseña?  <a href="/" className="text-white hover:text-blue-light font-semibold ml-1"> Recuperar</a>
+                </div>
+
+        </div>
+    )
+}
