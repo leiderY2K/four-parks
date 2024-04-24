771d2ec804938f3d4abf10c02df62d0b356207eb
CristianSGT89 <cristiansgt089@gmail.com>
2024-04-24 08:40:05 -0500

    Se elimina el jjwt para poder usar el back

--- backend/src/main/java/com/project/layer/Persistence/Repository/IUserRepository.java
+++ backend/src/main/java/com/project/layer/Persistence/Repository/IUserRepository.java
@@ -1,7 +1,6 @@
 package com.project.layer.Persistence.Repository;
 
 import com.project.layer.Persistence.Entity.User;
-import com.project.layer.Persistence.Entity.UserDetail;
 import com.project.layer.Persistence.Entity.UserId;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,11 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface IUserRepository extends JpaRepository<User, UserId> {
     
     
-    
-
-    static void save(UserDetail user) {
-        
-    }
-
     //Optional<User> findByUsername(String username);
 }
--- backend/src/main/java/com/project/layer/Services/Access/LoginRes.java
+++ backend/src/main/java/com/project/layer/Services/Access/LoginRes.java
@@ -1,15 +1,13 @@
 package com.project.layer.Services.Access;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthResponse {
-    String token;
+public class LoginRes {
+    private String username;
+    private String token;
 }
-
