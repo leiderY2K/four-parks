2117c4293fbd6842fd31020ec2b3ab2ba8b61108
CristianGT089 <cristiansgt089@gmail.com>
2024-04-27 12:50:29 -0500

    fix: :lock: Se agrega configuracion de CORS
    Faltaba metodo que permitia configuracion de los CORS, se ajusta para que solo acepte informacion del puerto de react

--- backend/src/main/java/com/project/layer/BackApplication.java
+++ backend/src/main/java/com/project/layer/BackApplication.java
@@ -2,11 +2,28 @@ package com.project.layer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+				registry
+					.addMapping("/**")
+					.allowedOrigins("http://localhost:5173")
+					.allowedMethods("*")
+					.allowedHeaders("*");
+			}
+		};
 	}
 
 }
