787b5618eda7cba1983035d49d9dcf0fa91e8386
CristianGT089 <cristiansgt089@gmail.com>
2024-05-11 17:51:38 -0500

    fix: :construction_worker: Se agrega el servicio de parametrizacion y se modifica la BD
    Se agregan los endpoints de agregar y eliminar parqueaderos y se ajustan startTime y endTime

--- backend/src/main/java/com/project/layer/Persistence/Entity/Schedule.java
+++ backend/src/main/java/com/project/layer/Persistence/Entity/Schedule.java
@@ -1,7 +1,5 @@
 package com.project.layer.Persistence.Entity;
 
-import java.sql.Time;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -20,13 +18,7 @@ public class Schedule {
     @Id
     @Column(name = "IDSCHEDULE", nullable = false)
     private int idSchedule;
-
-    @Column(name = "STARTTIME", nullable = false)
-    private Time startTime;
-
-    @Column(name = "ENDTIME", nullable = false)
-    private Time endTime;
-
+    
     @Column(name = "SCHEDULETYPE", nullable = false)
     private String scheduleType;
 
