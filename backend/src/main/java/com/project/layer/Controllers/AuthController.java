42c91311ea15705f0366a580559d7128d8abc914
daniel <dmcuestaf@udistrital.edu.co>
2024-05-13 17:57:57 -0500

    fix: :bug: se siguen corrigiendo errores con el merge del proyecto
    se va subir los cambios a la rama respectiva
    
    none

--- backend/src/main/java/com/project/layer/Controllers/AuthController.java
+++ backend/src/main/java/com/project/layer/Controllers/AuthController.java
@@ -65,7 +65,7 @@ public class AuthController {
             throw new CustomException(("El  nombre de usuario ya se encuentra registrado"));
         }
         AuthResponse respuesta = authService.register(request);
-        List<String> messages = Arrays.asList("Registro",respuesta.getContra());
+        List<String> messages = Arrays.asList("email",respuesta.getContra());
         mailservice.sendMail("dmcuestaf@udistrital.edu.co", "Bienvenido a four-parks Colombia", messages);
         return ResponseEntity.ok(respuesta);
     }
--- backend/src/main/java/com/project/layer/Services/Reservation/ReservationService.java
+++ backend/src/main/java/com/project/layer/Services/Reservation/ReservationService.java
@@ -32,48 +32,48 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class ReservationService {
-    
+
     private final IReservationRepository reservationRepository;
-    private final IParkingRepository parkingRepository;
     private final IParkingSpaceRepository parkingSpaceRepository;
+    private final IParkingRepository parkingRepository;
     private final IUserRepository userRepository;
     private final IRateRepository rateRepository;
     private final MailService mailService;
 
-
-    
     public List<Reservation> getReservationsByClientId(UserReservationRequest urRequest) {
-        return reservationRepository.findAllByClientId(urRequest.getClientId().getIdUser(), urRequest.getClientId().getIdDocType(),urRequest.getStatus() );
+        return reservationRepository.findAllByClientId(urRequest.getClientId().getIdUser(),
+                urRequest.getClientId().getIdDocType(), urRequest.getStatus());
     }
-    
+
     @Transactional
     public String startReservation(StartReservationRequest reservationRequest) throws MessagingException {
-        
+
         Date sqlDate = Date.valueOf(LocalDate.now());
-        
-        System.out.println("La fecha: "+reservationRequest.getDateRes());
-        
+
+        System.out.println("La fecha: " + reservationRequest.getDateRes());
+
         Time startTime = Time.valueOf(reservationRequest.getStartTimeRes().toLocalTime().minusMinutes(59));
         Time entTime = Time.valueOf(reservationRequest.getEndTimeRes().toLocalTime().plusMinutes(59));
         List<Integer> busyParkingSpaces = reservationRepository.findBusyParkingSpaces(
-            reservationRequest.getCityId(),
-            reservationRequest.getParkingId(),
-            reservationRequest.getVehicleType(),
-            reservationRequest.getDateRes(),
-            startTime,
-            entTime
-        );
+                reservationRequest.getCityId(),
+                reservationRequest.getParkingId(),
+                reservationRequest.getVehicleType(),
+                reservationRequest.getDateRes(),
+                startTime,
+                entTime);
 
-        System.out.println("Numero de parqueaderos ocupados en ese intervalo de tiempo: " + busyParkingSpaces.toString());
-        List<ParkingSpace> parkingSpaces = parkingSpaceRepository.findAllByParking(reservationRequest.getParkingId(), reservationRequest.getCityId());
+        System.out
+                .println("Numero de parqueaderos ocupados en ese intervalo de tiempo: " + busyParkingSpaces.toString());
+        List<ParkingSpace> parkingSpaces = parkingSpaceRepository.findAllByParking(reservationRequest.getParkingId(),
+                reservationRequest.getCityId());
 
         ParkingSpace selectedParkingSpace = null;
         boolean wasFoundAnyParkingSpace = false;
         System.out.println("Espacio libre!");
-    
+
         for (ParkingSpace parkingSpace : parkingSpaces) {
             if (!busyParkingSpaces.contains(parkingSpace.getParkingSpaceId().getIdParkingSpace()) &&
-                parkingSpace.getIdVehicleType().equals(reservationRequest.getVehicleType())) {
+                    parkingSpace.getIdVehicleType().equals(reservationRequest.getVehicleType())) {
                 selectedParkingSpace = parkingSpace;
                 System.out.println(selectedParkingSpace.toString());
                 wasFoundAnyParkingSpace = true;
@@ -81,76 +81,81 @@ public class ReservationService {
             }
         }
 
-        if(!wasFoundAnyParkingSpace){
-            return  "No hay espacios disponibles";
+        if (!wasFoundAnyParkingSpace) {
+            return "No hay espacios disponibles";
         }
 
         User client = userRepository.getReferenceById(reservationRequest.getClientId());
-        List<String> reserva = Arrays.asList("Reserva",
-            reservationRequest.getDateRes().toString(),
-            reservationRequest.getStartTimeRes().toString(),
-            reservationRequest.getEndTimeRes().toString(),
-            reservationRequest.getLicensePlate(),
-            client.getFirstName()+" "+client.getLastName(),
-            reservationRequest.getClientId().getIdUser(),
-            reservationRequest.getClientId().getIdDocType(),
-            reservationRequest.getCityId(),
-            Integer.toString(reservationRequest.getParkingId()), 
-            reservationRequest.getVehicleType());
-        
+
         Reservation reservation = Reservation.builder()
-            .dateRes(reservationRequest.getDateRes())
-            .startTimeRes(reservationRequest.getStartTimeRes())
-            .endTimeRes(reservationRequest.getEndTimeRes())
-            .creationDateRes(sqlDate)
-            .totalRes(0)
-            .licensePlate(reservationRequest.getLicensePlate())
-            .client(client)
-            .vehicleType(reservationRequest.getVehicleType())
-            .parkingSpace(selectedParkingSpace)
-            .status(ResStatus.PENDING.getId())
-            .build();
+                .dateRes(reservationRequest.getDateRes())
+                .startTimeRes(reservationRequest.getStartTimeRes())
+                .endTimeRes(reservationRequest.getEndTimeRes())
+                .creationDateRes(sqlDate)
+                .totalRes(0)
+                .licensePlate(reservationRequest.getLicensePlate())
+                .client(client)
+                .vehicleType(reservationRequest.getVehicleType())
+                .parkingSpace(selectedParkingSpace)
+                .status(ResStatus.PENDING.getId())
+                .build();
+        Optional<Parking> parking = parkingRepository.findById(reservation.getParkingSpace().getParkingSpaceId().getIdParking());
+        List<String> reserva = Arrays.asList("email",
+                Integer.toString(reservation.getIdReservation()),
+                reservation.getDateRes().toString(),
+                reservation.getStartTimeRes().toString(),
+                reservation.getEndTimeRes().toString(),
+                reservation.getLicensePlate(),
+                client.getFirstName() + " " + client.getLastName(),
+                reservationRequest.getClientId().getIdUser(),
+                reservationRequest.getClientId().getIdDocType(),
+                reservationRequest.getCityId(),
+                parking.get().getNamePark(),
+                reservationRequest.getVehicleType());
+
         reservationRepository.save(reservation);
-        mailService.sendMail("dmcuestaf@udistrital.edu.co", "[Four-parks] Informaciòn de su reserva",reserva);
+        mailService.sendMail("dmcuestaf@udistrital.edu.co", "[Four-parks] Informaciòn de su reserva", reserva);
         return "¡La reserva se realizo exitosamente!";
     }
-    
+
     @Transactional
-    public String confirmReservation(){
+    public String confirmReservation() {
 
         Time actualTime = Time.valueOf(LocalTime.now().minusMinutes(5));
-        Time postActualTime = Time.valueOf(LocalTime.now().plusMinutes(40));;
+        Time postActualTime = Time.valueOf(LocalTime.now().plusMinutes(40));
+        ;
 
         List<Reservation> reservations = reservationRepository.findByStartTime(actualTime, postActualTime);
 
         if (reservations.isEmpty()) {
             return "No se encontró ninguna reserva con el ID proporcionado.";
         }
-        
+
         for (Reservation reservation : reservations) {
             long totalSeconds;
-            
+
             LocalTime startTime = reservation.getStartTimeRes().toLocalTime();
             LocalTime endTime = reservation.getEndTimeRes().toLocalTime();
-    
+
             totalSeconds = Duration.between(startTime, endTime).getSeconds();
             System.out.println("Aun no se ha acabado la reserva");
-    
-            //Se hace de esta manera ya que en la BD el cobro se hace por horas
+
+            // Se hace de esta manera ya que en la BD el cobro se hace por horas
             float totalHours = totalSeconds / 3600.0f;
-            
+
             int rate = rateRepository.getHourCostByParkingSpace(
-                reservation.getParkingSpace().getParkingSpaceId().getIdParking(),
-                reservation.getParkingSpace().getParkingSpaceId().getIdCity(),
-                reservation.getVehicleType(),
-                reservation.getParkingSpace().isUncovered()
-            );
-    
+                    reservation.getParkingSpace().getParkingSpaceId().getIdParking(),
+                    reservation.getParkingSpace().getParkingSpaceId().getIdCity(),
+                    reservation.getVehicleType(),
+                    reservation.getParkingSpace().isUncovered());2
+
             float totalCost = totalHours * rate;
-    
-            // Se debe realizar el pago -------------------------------------------------------------------------------------------------------------
-    
-            // Se debe enviar email de correo -------------------------------------------------------------------------------------------------------
+
+            // Se debe realizar el pago
+            // -------------------------------------------------------------------------------------------------------------
+
+            // Se debe enviar email de correo
+            // -------------------------------------------------------------------------------------------------------
             reservation.setStatus(ResStatus.CONFIRMED.getId());
             reservation.setTotalRes(totalCost);
 
@@ -162,7 +167,7 @@ public class ReservationService {
 
     @Transactional
     public String checkInReservation(int idReservation) {
-        
+
         Optional<Reservation> optionalReservation = reservationRepository.findById(idReservation);
 
         if (optionalReservation.isEmpty()) {
@@ -173,13 +178,13 @@ public class ReservationService {
         reservation.setStatus(ResStatus.IN_PROGRESS.getId());
 
         reservationRepository.save(reservation);
-        
+
         return "¡Su reserva ha comenzado!";
     }
 
     @Transactional
     public String cancelReservation(int idReservation) {
-        
+
         Optional<Reservation> optionalReservation = reservationRepository.findById(idReservation);
 
         if (optionalReservation.isEmpty()) {
@@ -187,21 +192,22 @@ public class ReservationService {
         }
 
         Reservation reservation = optionalReservation.get();
-        
-        if (!(reservation.getStatus().equals(ResStatus.PENDING.getId()) || reservation.getStatus().equals(ResStatus.CONFIRMED.getId()))) {
+
+        if (!(reservation.getStatus().equals(ResStatus.PENDING.getId())
+                || reservation.getStatus().equals(ResStatus.CONFIRMED.getId()))) {
             return "¡La reserva no puede ser cancelada!";
         }
-        
+
         int cancellationCost = rateRepository.getCancellationCostByParkingSpace(
-            reservation.getParkingSpace().getParkingSpaceId().getIdParking(),
-            reservation.getParkingSpace().getParkingSpaceId().getIdCity(),
-            reservation.getVehicleType(),
-            reservation.getParkingSpace().isUncovered()
-        );
+                reservation.getParkingSpace().getParkingSpaceId().getIdParking(),
+                reservation.getParkingSpace().getParkingSpaceId().getIdCity(),
+                reservation.getVehicleType(),
+                reservation.getParkingSpace().isUncovered());
 
         System.out.println("Costo de tarifa: " + cancellationCost);
 
-        //Se debe realizar el cargo por cancelación -------------------------------------------------------------------------------------------------------------
+        // Se debe realizar el cargo por cancelación
+        // -------------------------------------------------------------------------------------------------------------
 
         reservation.setStatus(ResStatus.CANCELLED.getId());
 
@@ -213,15 +219,15 @@ public class ReservationService {
     @Transactional
     public String checkOutReservation(int idReservation) {
         Optional<Reservation> optionalReservation = reservationRepository.findById(idReservation);
-    
+
         if (optionalReservation.isEmpty()) {
             return "No se encontró ninguna reserva con el ID proporcionado.";
-        }    
-        
+        }
+
         Reservation reservation = optionalReservation.get();
         System.out.println("La reserva es: " + reservation.toString());
-        
-        long extraSeconds; 
+
+        long extraSeconds;
 
         LocalTime endTime = reservation.getEndTimeRes().toLocalTime();
 
@@ -229,25 +235,24 @@ public class ReservationService {
             System.out.println("Se hace cobro por pasarse de tiempo");
 
             extraSeconds = Duration.between(endTime, LocalTime.now()).getSeconds();
-            
-            //Se hace de esta manera ya que en la BD el cobro se hace por horas
+
+            // Se hace de esta manera ya que en la BD el cobro se hace por horas
             float extraHours = extraSeconds / 3600.0f;
-            
+
             int rate = rateRepository.getHourCostByParkingSpace(
-                reservation.getParkingSpace().getParkingSpaceId().getIdParking(),
-                reservation.getParkingSpace().getParkingSpaceId().getIdCity(),
-                reservation.getVehicleType(),
-                reservation.getParkingSpace().isUncovered()
-            );
-    
+                    reservation.getParkingSpace().getParkingSpaceId().getIdParking(),
+                    reservation.getParkingSpace().getParkingSpaceId().getIdCity(),
+                    reservation.getVehicleType(),
+                    reservation.getParkingSpace().isUncovered());
+
             float extraCost = extraHours * rate;
 
-            //Realizar pago automatico por minutos extra
-    
+            // Realizar pago automatico por minutos extra
+
             System.out.println("--------------------------- Horas transcurridas: " + extraHours);
             reservation.setTotalRes(reservation.getTotalRes() + extraCost);
         }
-        
+
         // Actualizar campos
         reservation.setStatus(ResStatus.COMPLETED.getId());
 
@@ -255,8 +260,7 @@ public class ReservationService {
         reservationRepository.save(reservation);
 
         return "¡Su salida ha sido exitosa!";
-    
+
     }
 
-    
 }
